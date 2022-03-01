7bed15f415379e414390b1f8369bf03da8b89e88
codewithshamim <shamimislamonline@gmail.com>
2022-03-01 18:26:18 +0600

    Done

--- script.js
+++ script.js
@@ -20,6 +20,9 @@ const search = () => {
     searchField.value = '';
     // add spinner 
     getSpinner('block');
+    const detailsCard = document.getElementById('details-section');
+    detailsCard.textContent = '';
+
 
 
     if (searchFieldValue === '') {
@@ -34,6 +37,7 @@ const search = () => {
             .then(datas => displayData(datas.data))
             .catch(error => console.log(error))
     }
+
 
 
     // get data -------
@@ -64,7 +68,7 @@ const search = () => {
                             <h5 class="card-title">Name: ${phone.phone_name}</h5>
                             <p class="card-text">Brand: ${phone.brand}</p>    
                         </div>
-                        <button onclick="loadDetails('${phone.slug}')" class="bg-info py-3 border-0 w-50 mx-auto rounded mb-3 fw-bold text-white">Details</button>
+                        <button onclick="loadDetails('${phone.slug}')" id='#home' class="bg-info py-3 border-0 w-50 mx-auto rounded mb-3 fw-bold text-white">Details</button>
                     </div>  
                      `
                 parentCard.appendChild(div);
@@ -94,6 +98,7 @@ const getDetails = (data) => {
     console.log(data.data.releaseDate);
     const details = data.data;
     const detailsCard = document.getElementById('details-section');
+    detailsCard.textContent = '';
     const div = document.createElement('div');
     div.classList.add('card');
     div.innerHTML =
@@ -103,16 +108,18 @@ const getDetails = (data) => {
             <img src="${details.image? details.image:'./images/pic.jgp'}" class="shadow w-100  img-fluid rounded-start" alt="...">
         </div>
         <div class="col-md-8">
-            <div class="card-body">
-                <h3 class="card-title">${details.brand? details.brand:'No'}</h3>
-                <p class="card-text">Release date: ${details.releaseDate? details.releaseDate:'No'}</p>
-                <div>
-                    <h4>Main Features:</h4>
-                    <p><span class= "fs-5 text-info">Chipset:</span> ${details.mainFeatures.chipSet? details.mainFeatures.chipSet:'No'}</p>
-                    <p><span class= "fs-5 text-info">Display size:</span> ${details.mainFeatures.displaySize? details.mainFeatures.displaySize:'No'}</p>
-                    <p><span class= "fs-5 text-info">Memory:</span> ${details.mainFeatures.memory? details.mainFeatures.memory:'No'}</p>       
-                    <p><span class= "fs-5 text-info">Storage:</span> ${details.mainFeatures.storage? details.mainFeatures.storage:'No'}</p>  
+            <div class="card-body  ms-4">
+                <h3 class="card-title text-center">${details.brand? details.brand:'No'}</h3>
+                <p class="card-text text-center text-primary">Release date: ${details.releaseDate? details.releaseDate:'No'}</p>
+                <div class="d-flex justify-content-around">
                     <div>
+                        <h4>Main Features:</h4>
+                        <p><span class= "fs-5 text-info">Chipset:</span> ${details.mainFeatures.chipSet? details.mainFeatures.chipSet:'No'}</p>
+                        <p><span class= "fs-5 text-info">Display size:</span> ${details.mainFeatures.displaySize? details.mainFeatures.displaySize:'No'}</p>
+                        <p><span class= "fs-5 text-info">Memory:</span> ${details.mainFeatures.memory? details.mainFeatures.memory:'No'}</p>       
+                        <p><span class= "fs-5 text-info">Storage:</span> ${details.mainFeatures.storage? details.mainFeatures.storage:'No'}</p>  
+                    </div>
+                    <div class='ms-4'>
                         <h4>Sensors: </h4>
                         <p><span class= "fs-5 text-info">1:</span> ${details.mainFeatures.sensors[0]? details.mainFeatures.sensors[0]:'No'}</p>    
                         <p><span class= "fs-5 text-info">2:</span> ${details.mainFeatures.sensors[1]? details.mainFeatures.sensors[1]:'No'}</p>    
@@ -123,7 +130,7 @@ const getDetails = (data) => {
                     </div>                        
                 </div>
 
-                <div>
+                <div class='mt-4'>
                     <h4>Other Features:</h4>
                     <p><span class="fs-5 text-danger">Bluetooth:</span> ${details.others.Bluetooth? details.others.Bluetooth:'No'}</p>
                     <p><span class="fs-5 text-danger">GPS:</span> ${details.others.GPS? details.others.GPS:'No'}</p>
